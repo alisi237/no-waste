62bf1f107e2f80639b711d9dce4ed012d349a111
alisi237 <alinasiol@icloud.com>
2022-11-08 15:45:12 +0100

    make rest calls great again

--- src/app/home-tab/services/home-tab-service.ts
+++ src/app/home-tab/services/home-tab-service.ts
@@ -9,13 +9,15 @@ import { Item } from '../models/home-tab-model';
 })
 export class HomeRestService {
 
-  private static readonly DATABASE_URL = 'http://localhost:6000/api/v1/groceries/';
+  private static readonly DATABASE_URL = 'http://localhost:8080/api/v1/groceries/';
 
   constructor(private http: HttpClient) {
   }
 
   addItem(item: Item): Observable<Item> {
     const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    console.log('test');
+    
     return this.http.post<Item>(HomeRestService.DATABASE_URL, {name: 'dingens', place: 'bummens'}, { headers })
       .pipe(catchError(err => {
         console.log('tbd', err);
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,6 @@ import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ProfilePage } from '../profile/components/profile.page';
-import { HomeTabPage } from '../home-tab/components/home-tab.page';
-import { ItemsComponent } from '../home-tab/modals/items/components/items.component';
-import { InventoryComponent } from '../home-tab/modals/inventory/components/inventory.component';
 
 
 
